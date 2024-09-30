5fd9c8a2b475045e929dc232eb543b29bd0e0ed3
verity <verity@cock.li>
2024-09-30 14:16:54 +0000

    blabla

---
+++ nixos/locale.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }
+  
+{
+  i18n.defaultLocale = "en_US.UTF8"
+  services.xserver.layout = "de"
+  console.keyMap = "de"
+}
---
+++ nixos/vpn.nix
@@ -0,0 +1,5 @@
+{pkgs, ... };
+
+{
+  services.mullvad-vpn.enable = true
+}
