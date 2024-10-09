527d6831af97431da4479b3e1ac5b30a7f856966
verity <verity@cock.li>
2024-10-09 17:46:59 +0200

    fsdfs

--- nixos/shell.nix
+++ nixos/shell.nix
@@ -2,16 +2,4 @@
 
 {
   programs.fish.enable = true;
-  environment.systemPackages = with pkgs; [
-    fishPlugins.done
-    fishPlugins.fzf-fish
-    fishPlugins.forgit
-    fishPlugins.hydro
-    fzf
-    fishPlugins.grc
-    grc
-    fishPlugins.z
-    fishPlugins.pure
-    fishPlugins.sponge
-  ];
 }
