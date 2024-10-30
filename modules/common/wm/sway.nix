41dab8e22c6619efbde553efe0f8468164a43054
veri-tty <>
2024-10-30 17:58:07 +0000

    workspace bindings in sway

--- modules/common/wm/sway.nix
+++ modules/common/wm/sway.nix
@@ -1,10 +1,13 @@
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ../wm
   ];
-  
+
   config = {
     ## Setting appropriate options so other modules can use them.
     os.wayland = true;
@@ -12,15 +15,15 @@
     environment.systemPackages = [
       pkgs.swaybg
     ];
-    
+
     ## Sway
     home-manager.users.${config.user} = {
       wayland.windowManager.sway = {
         enable = true;
-	
+
         ## Enable XWayland
         xwayland = true;
-        
+
         ## Properly expose env to DBus
         systemd.enable = true;
         wrapperFeatures = {
@@ -35,9 +38,7 @@
           export _JAVA_AWT_WM_NOPARENTING=1
         '';
 
-  
         config = {
-
           ## Set modifier
           modifier = "Mod4";
 
@@ -52,11 +53,9 @@
             };
           };
 
-
-
           ## Fonts
           fonts = {
-            names = [ config.os.fonts.mono.regular ];
+            names = [config.os.fonts.mono.regular];
           };
 
           ## Terminal
@@ -68,84 +67,89 @@
           window.titlebar = false;
           floating.titlebar = false;
           gaps.inner = 8;
-	  gaps.outer = 2;
+          gaps.outer = 2;
 
           ## Launcher
           menu = "${config.os.launcher.pkg}/bin/${config.os.launcher.name} ${config.os.launcher.args}";
 
           ## We'll start the bar through dbus
-	  bars = [
+          bars = [
             {
               command = "waybar";
             }
           ];
-	  startup = [
-	    {
-	    command = "swaybg --image ${config.wallpaper}"; 
+          startup = [
+            {
+              command = "swaybg --image ${config.wallpaper}";
             }
-	  ]; 
-          colors =
-              let
-                background = config.theme.colors.base00;
-                inactiveBackground = config.theme.colors.base01;
-                border = config.theme.colors.base01;
-                inactiveBorder = config.theme.colors.base01;
-                text = config.theme.colors.base07;
-                inactiveText = config.theme.colors.base04;
-                urgentBackground = config.theme.colors.base08;
-                indicator = "#00000000";
-              in
-              {
-                background = config.theme.colors.base00;
-                focused = {
-                  inherit
-                    background
-                    indicator
-                    text
-                    border
-                    ;
-                  childBorder = background;
-                };
-                focusedInactive = {
-                  inherit indicator;
-                  background = inactiveBackground;
-                  border = inactiveBorder;
-                  childBorder = inactiveBackground;
-                  text = inactiveText;
-                };
-                # placeholder = { };
-                unfocused = {
-                  inherit indicator;
-                  background = inactiveBackground;
-                  border = inactiveBorder;
-                  childBorder = inactiveBackground;
-                  text = inactiveText;
-                };
-                urgent = {
-                  inherit text indicator;
-                  background = urgentBackground;
-                  border = urgentBackground;
-                  childBorder = urgentBackground;
-                };
-              };
+          ];
+          colors = let
+            background = config.theme.colors.base00;
+            inactiveBackground = config.theme.colors.base01;
+            border = config.theme.colors.base01;
+            inactiveBorder = config.theme.colors.base01;
+            text = config.theme.colors.base07;
+            inactiveText = config.theme.colors.base04;
+            urgentBackground = config.theme.colors.base08;
+            indicator = "#00000000";
+          in {
+            background = config.theme.colors.base00;
+            focused = {
+              inherit
+                background
+                indicator
+                text
+                border
+                ;
+              childBorder = background;
+            };
+            focusedInactive = {
+              inherit indicator;
+              background = inactiveBackground;
+              border = inactiveBorder;
+              childBorder = inactiveBackground;
+              text = inactiveText;
+            };
+            # placeholder = { };
+            unfocused = {
+              inherit indicator;
+              background = inactiveBackground;
+              border = inactiveBorder;
+              childBorder = inactiveBackground;
+              text = inactiveText;
+            };
+            urgent = {
+              inherit text indicator;
+              background = urgentBackground;
+              border = urgentBackground;
+              childBorder = urgentBackground;
+            };
+          };
           ## Keybindings
           keybindings = let
             mod = config.home-manager.users.${config.user}.wayland.windowManager.sway.config.modifier;
             menu = config.home-manager.users.${config.user}.wayland.windowManager.sway.config.menu;
             passwordManager = config.os.passwordManager;
             screenshot = config.os.screenshot;
-          in lib.mkOptionDefault {
-            "${mod}+r" = "reload";
-            "${mod}+q" = "kill";
-            "${mod}+f" = "fullscreen";
-            "${mod}+Ctrl+f" = "focus mode_toggle";
-            "${mod}+a" = "exec ${menu}";
-            #"${mod}+Shift+p" = "exec ${passwordManager}";
-            "Print" = "exec ${screenshot}/bin/screenshot";
+          in
+            lib.mkOptionDefault {
+              "${mod}+r" = "reload";
+              "${mod}+c" = "exec vscode";
+              "${mod}+f" = "exec firefox";
+              "${mod}+q" = "kill";
+              "${mod}+f" = "fullscreen";
+              "${mod}+Ctrl+f" = "focus mode_toggle";
+              "${mod}+a" = "exec ${menu}";
+              #"${mod}+Shift+p" = "exec ${passwordManager}";
+              "Print" = "exec ${screenshot}/bin/screenshot";
+            };
+          assigns = {
+            "1: terminal" = [{class = "^Kitty$";}];
+            "2: code" = [{class = "^VSCode$";}];
+            "3: web" = [{class = "^Firefox$";}];
           };
         };
       };
     };
   };
 }
-
